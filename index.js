e39116e7d8d19675722b0de71bf2a5c0c09eda8c
Ana Rosario <arosario.mrd@gmail.com>
2022-11-09 01:02:22 -0500

    commit tree 2a892e88d255dd1f0a51dc89a53fd3029f366935

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
-}
+
+  function fetchBooks() {
+    return fetch("https://anapioficeandfire.com/api/books")
+   .then(results => results.json())
+   .then( results => renderBooks(results))
+  }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -12,6 +14,5 @@ function renderBooks(books) {
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+
+
